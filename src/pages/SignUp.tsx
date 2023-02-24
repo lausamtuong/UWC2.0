a5539ff2ecc22a119e4ea3be66e989a97eb30b5f
lausamtuong <lausamtuong@gmail.com>
2023-02-24 07:58:08 +0700

    init pages

---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,11 @@
+export default function SignUp() {
+  type SignUp={
+    email:string,
+    password:string,
+  }
+    return (
+      <h1 className="text-[100px] text-red-500 font-bold underline">
+        Hello world!s
+      </h1>
+    )
+  }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <h1 className="text-[100px] text-red-500 font-bold underline">
-      Hello world!s
+      Login
     </h1>
   )
 }
