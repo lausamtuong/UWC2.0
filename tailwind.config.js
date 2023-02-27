125d1f5a926a3f0f4a90c40f245da3b9219e3bd0
unknown <danggiathinhgocong@gmail.com>
2023-02-27 21:18:17 +0700

    UI/login,signup

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
@@ -18,7 +18,10 @@ module.exports = {
         "divider-color": "#e4e4e4",
         "nd-text-color": "#808191",
       },
-      fontFamily: { poppins: "Poppins", "bold-37": "Manrope" },
+      fontFamily: {
+        poppins: "Poppins",
+        manrope: "Manrope",
+      },
       borderRadius: { base: "10px" },
     },
     fontSize: { sm: "14px", base: "16px", lg: "37px" },
