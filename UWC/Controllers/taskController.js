a70fdbe2a3a281cbdeacc31b27de98fb9dd484e5
lausamtuong <lausamtuong@gmail.com>
2023-04-20 09:53:50 +0700

    createRooute

--- UWC/Controllers/taskController.js
+++ UWC/Controllers/taskController.js
@@ -3,7 +3,7 @@ exports.createTask = async (req, res) => {
   const jsonData = fs.readFileSync('./Data/TaskJanitor.json', 'utf-8');
   const data = JSON.parse(jsonData);
   const newArray = data.concat(req.body)
-  console.log(123)
+ 
   fs.writeFileSync("./Data/TaskJanitor.json", JSON.stringify(newArray), (err) => {
     if (err) {
       console.error(err);
@@ -14,4 +14,11 @@ exports.createTask = async (req, res) => {
   console.log("Data written to file");
  // res.status(201).json(dataArray);
 };
+exports.createRoute= async (req,res)=>{
+  const jsonData = fs.readFileSync('./Data/MCPs.json', 'utf-8');
+  const data = JSON.parse(jsonData);
+  console.log(data)
+  const response= data.filter((item)=>item.id==req.body.data)
+  res.status(200).json(response)
+}
 
--- UWC/Routes/taskRoutes.js
+++ UWC/Routes/taskRoutes.js
@@ -8,7 +8,7 @@ router
 	.route('/')
 	//.get( taskController.getAllTask)
 	.post(taskController.createTask);
-
+router.route('/route').post(taskController.createRoute)
 
 
 module.exports = router;
